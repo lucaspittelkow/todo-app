d363b3c1bd0823214c48860c441abe8415554c36
lucaspittelkow <lucaspittelkow@gmail.com>
2021-05-10 02:01:28 -0300

    Added sorting

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
 import './styles/Tasks.css';
 
 export default function Tasks(props){
 
     return (
         <ul>
-            {props.data.map(item => {
+            {props.data.sort((x, y) => {
+                return (x.done === y.done)? 0 : x.done ? 1 : -1;
+            }).map(item => {
                 return(
                     <li key={item.key} className={item.done && "done"}>
                         <input 
